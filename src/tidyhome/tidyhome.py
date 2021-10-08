adf643368c822557c1647a64e8bfe75db3b4b528
Charles Graham <charles.graham@summitllc.us>
2021-10-08 13:33:14 -0400

    modified docstrings and import statements

--- src/tidyhome/tidyhome.py
+++ src/tidyhome/tidyhome.py
@@ -1,4 +1,4 @@
-"""a package that simplifies the process of retrieving HMDA data from the HMDA Platform API
+"""A package that simplifies the process of retrieving HMDA data from the HMDA Platform API
 
 Tidyhome provides access to data on US home mortgages from CFPB's Home Mortgage Disclosure Act (HMDA)
 Platform API via several functions. These functions make the proper API request (given your desired
@@ -7,14 +7,19 @@ access data on individual loans, summaries of loans, and lending institutions.
 
     Usage examples:
 
-    tidyhome.get_institutions(2018, ["DC", "Md", "va"])
+    >>> tidyhome.get_institutions(2018, ["DC", "Md", "va"])
+    
         -returns a DataFrame containing data on lending institutions that filed HMDA data in 2018 in 
          DC, Maryland, and Virginia.
-    tidyhome.get_loans(2019, "dc", [Action.INCOMPLETE, Action.PREAPPROVED], [Race.BLACK, Race.WHITE])
+
+    >>> tidyhome.get_loans(2019, "dc", [tidyhome.Action.INCOMPLETE, tidyhome.Action.PREAPPROVED], [tidyhome.Race.BLACK, tidyhome.Race.WHITE])
+        
         -returns a DataFrame containing HMDA data on all individual loans in 2019 in DC where the file 
          was closed for incompleteness or the loan was preapproved, and the reported races of 
          applicants/borrowers were black or white.
-    tidyhome.get_aggregations(2020, "DC" , races=Race.UNAVAILABLE)
+
+    >>> tidyhome.get_aggregations(2020, "DC" , races=tidyhome.Race.UNAVAILABLE)
+
         -returns a DataFrame containing HMDA data on the total quantity and sum of all loans reported 
          in 2020 in DC where the reported race of applicants/borrowers was unavailable.
 """
@@ -29,7 +34,7 @@ from typing import Dict, Iterable, List, Optional, Union
 from enum import Enum
 import pandas as pd
 import requests
-import lookup
+from tidyhome import lookup
 
 # Limit what gets imported via the 'import *' statement
 __all__ = [
